e44d0c228cce2fb85f662d9f3b759bebe25d65e4
Hkpriddy <hapr9603@colorado.edu>
2025-02-11 11:04:41 -0700

    Add files via upload

--- Lab/Lab5.py
+++ Lab/Lab5.py
@@ -1,1 +1,194 @@
 #BeepBoop
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#For Newton's method - We will always converge to a unique root in the neighborhood of the root when the following #conditions are met:
+# 1. f'(x) >> 0 
+# 2. Root is unique in it's neighborhood
+# 3. Size of neighborhood is proportionally smaller based on f'(x)
+
+def bisect_method(f,a,b,tol,nmax,vrb=False):
+    #Bisection method applied to f between a and b
+
+    # Initial values for interval [an,bn], midpoint xn
+    an = a; bn=b; n=0;
+    xn = (an+bn)/2;
+    # Current guess is stored at rn[n]
+    rn=np.array([xn]);
+    r=xn;
+    ier=0;
+
+    if vrb:
+        print("\n Bisection method with nmax=%d and tol=%1.1e\n" % (nmax, tol));
+
+    # The code cannot work if f(a) and f(b) have the same sign.
+    # In this case, the code displays an error message, outputs empty answers and exits.
+    if f(a)*f(b)>=0:
+        print("\n Interval is inadequate, f(a)*f(b)>=0. Try again \n")
+        print("f(a)*f(b) = %1.1f \n" % f(a)*f(b));
+        r = None;
+        return r
+    else:
+        #If f(a)f(b), we proceed with the method.
+        if vrb:
+            print("\n|--n--|--an--|--bn--|----xn----|-|bn-an|--|---|f(xn)|---|");
+
+        while n<=nmax:
+            if vrb:
+                print("|--%d--|%1.4f|%1.4f|%1.8f|%1.8f|%1.8f|" % (n,an,bn,xn,bn-an,np.abs(f(xn))));
+
+            # Bisection method step: test subintervals [an,xn] and [xn,bn]
+            # If the estimate for the error (root-xn) is less than tol, exit
+            if (bn-an)<2*tol: # better test than np.abs(f(xn))<tol
+                ier=1;
+                break;
+
+            # If f(an)*f(xn)<0, pick left interval, update bn
+            if f(an)*f(xn)<0:
+                bn=xn;
+            else:
+                #else, pick right interval, update an
+                an=xn;
+
+            # update midpoint xn, increase n.
+            n += 1;
+            xn = (an+bn)/2;
+            rn = np.append(rn,xn);
+
+    # Set root estimate to xn.
+    r=xn;
+
+    return r, rn;
+
+
+def NewBisection(f,a,b,nmax, tol = 10**(-1), vrb = True):
+    an = a; bn=b; n=0;
+    xn = (an+bn)/2;
+    # Current guess is stored at rn[n]
+    rn=np.array([xn]);
+    r=xn;
+    ier=0;
+    if vrb:
+        print("\n Bisection method with nmax=%d and tol=%1.1e\n" % (nmax, tol));
+
+    # The code cannot work if f(a) and f(b) have the same sign.
+    # In this case, the code displays an error message, outputs empty answers and exits.
+    if f(a)*f(b)>=0:
+        print("\n Interval is inadequate, f(a)*f(b)>=0. Try again \n")
+        print("f(a)*f(b) = %1.1f \n" % f(a)*f(b));
+        r = None;
+        return r
+    else:
+        #If f(a)f(b), we proceed with the method.
+        if vrb:
+            print("\n|--n--|--an--|--bn--|----xn----|-|bn-an|--|---|f(xn)|---|");
+
+        while n<=nmax:
+            if vrb:
+                print("|--%d--|%1.4f|%1.4f|%1.8f|%1.8f|%1.8f|" % (n,an,bn,xn,bn-an,np.abs(f(xn))));
+
+            # Bisection method step: test subintervals [an,xn] and [xn,bn]
+            # If the estimate for the error (root-xn) is less than tol, exit
+            if (bn-an)<2*tol: # better test than np.abs(f(xn))<tol
+                ier=1;
+                break;
+
+            # If f(an)*f(xn)<0, pick left interval, update bn
+            if f(an)*f(xn)<0:
+                bn=xn;
+            else:
+                #else, pick right interval, update an
+                an=xn;
+
+            # update midpoint xn, increase n.
+            n += 1;
+            xn = (an+bn)/2;
+            rn = np.append(rn,xn);
+    	
+        r = xn
+        if vrb:
+            print("End of Bisection method - we have reached tolerance for the basin of convergence")
+
+        return r, rn;
+
+#The only change to the Bisection method is that we have a predetermined tolerance that is far higher than previous #inputs. We only need to approach a neighborhood before doing the newton mthod.
+
+
+
+def newton_method(f,df,x0,tol,nmax,verb=False):
+    #newton method to find root of f starting at guess x0
+
+    #Initialize iterates and iterate list
+    xn=x0;
+    rn=np.array([x0]);
+    # function evaluations
+    fn=f(xn); dfn=df(xn);
+    nfun=2; #evaluation counter nfun
+    dtol=1e-10; #tolerance for derivative (being near 0)
+
+    if abs(dfn)<dtol:
+        #If derivative is too small, Newton will fail. Error message is
+        #displayed and code terminates.
+        if verb:
+            fprintf('\n derivative at initial guess is near 0, try different x0 \n');
+    else:
+        n=0;
+        if verb:
+            print("\n|--n--|----xn----|---|f(xn)|---|---|f'(xn)|---|");
+
+        #Iteration runs until f(xn) is small enough or nmax iterations are computed.
+
+        while n<=nmax:
+            if verb:
+                print("|--%d--|%1.8f|%1.8f|%1.8f|" %(n,xn,np.abs(fn),np.abs(dfn)));
+
+            pn = - fn/dfn; #Newton step
+            if np.abs(pn)<tol or np.abs(fn)<2e-15:
+                break;
+
+            #Update guess adding Newton step
+            xn = xn + pn;
+
+            # Update info and loop
+            n+=1;
+            rn=np.append(rn,xn);
+            dfn=df(xn);
+            fn=f(xn);
+            nfun+=2;
+
+        r=xn;
+
+        if n>=nmax:
+            print("Newton method failed to converge, niter=%d, nfun=%d, f(r)=%1.1e\n'" %(n,nfun,np.abs(fn)));
+        else:
+            print("Newton method converged succesfully, niter=%d, nfun=%d, f(r)=%1.1e" %(n,nfun,np.abs(fn)));
+
+
+
+    return (r,rn,nfun);
+	
+	
+
+
+def FrankenMethod(f,df,a, b,tol,nmax,verb=False):
+	
+     (r1,r1n)=NewBisection(fun,a,b,nmax)
+     newton_method(f,df,r1,tol,nmax,verb)
+
+
+def fun(x):
+    return np.exp(x**2 + 7*x - 30) - 1;
+def dfun(x):
+    return (2*x + 7)*np.exp(x**2 + 7*x - 30);
+
+
+
+bisect_method(fun,2,4.5,10**(-10),100,True)
+newton_method(fun,dfun,4.5,10**(-10),50,True)
+FrankenMethod(fun,dfun,2,4.5, 10**(-10),30,True)
+
+
+# Bisection: 34 Iterations to converge to 3
+# Newton: 26 iterations to converge to 3
+# Franken: Convergence in 8 iterations!!!
